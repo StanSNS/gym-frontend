1c4118f954b759c9c67f798ab35c950f35838b7a
Stanimir Sergev <stanimir.sergev@hubparking.com>
2024-04-02 17:17:50 +0300

    add user table some.

--- src/Component/Admin/Dashboard/Dashboard.jsx
+++ src/Component/Admin/Dashboard/Dashboard.jsx
@@ -31,6 +31,31 @@ const Dashboard = () => {
         colors: ['#333', '#F86C6B'],
     };
 
+    const UserData = [
+        {
+            id: 1,
+            firstName: 'John',
+            middleName: 'Doe',
+            lastName: 'Smith',
+            email: 'john.doe@example.com',
+            orders: 10,
+            revenue: 500,
+            firstLogin: '2023-01-01',
+            lastLogin: '2024-03-31',
+        },
+        {
+            id: 2,
+            firstName: 'Jane',
+            middleName: 'Marie',
+            lastName: 'Johnson',
+            email: 'jane.johnson@example.com',
+            orders: 15,
+            revenue: 750,
+            firstLogin: '2022-12-15',
+            lastLogin: '2024-04-01',
+        },
+        // Add more mock data if needed
+    ];
 
     return (
         <div>
@@ -78,6 +103,41 @@ const Dashboard = () => {
                         <PieChart data={Orders}/>
                     </div>
                 </div>
+            </div>
+
+            <div className="table-container">
+                <table className="myTable">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Middle Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Orders</th>
+                        <th>Revenue</th>
+                        <th>First Login</th>
+                        <th>Last Login</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {UserData.map((user, index) => (
+                        <tr className={index % 2 === 0 ? 'even' : 'odd'}>
+                            <td>{user.id}</td>
+                            <td>{user.firstName}</td>
+                            <td>{user.middleName}</td>
+                            <td>{user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td>{user.orders}</td>
+                            <td>{user.revenue}</td>
+                            <td>{user.firstLogin}</td>
+                            <td>{user.lastLogin}</td>
+                            <td>Actions</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
