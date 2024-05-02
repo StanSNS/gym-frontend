841f1e5aa9b3497e111d4c1d5963c19d5226d949
StanSNS <stanimirsergev159@gmail.com>
2024-05-02 11:33:38 +0300

    about done almost

--- src/Component/About/MoreInfo/MoreInfo.jsx
+++ src/Component/About/MoreInfo/MoreInfo.jsx
@@ -15,26 +15,27 @@ function MoreInfo() {
 
     return (
         <div className="myWhiteColor">
-            <h1 className="fw-bolder text-center"><IoIosInformationCircle className="mb-2 me-2 myGreenBlueColor"/>Още информация</h1>
+            <h1 className="fw-bolder text-center"><IoIosInformationCircle className="mb-2 me-2 myGreenBlueColor"/>Още
+                информация</h1>
             <div className="moreDetails">
 
                 <div className="contactInfo">
                     <span className="fs-3 fw-bolder"><FaBusinessTime className="mb-1 me-2 myGreenBlueColor"/>Работно време:</span>
                     <span className="fs-4 fw-medium">
-                        <BsFillCalendarWeekFill className="mb-1 me-2 myGreenBlueColor"/>От понеделник
+                        <BsFillCalendarWeekFill className="mb-2 me-2 myGreenBlueColor"/>От понеделник
                         <span className="ms-2 me-2">-</span>
-                        <BsFillCalendarWeekFill className="mb-1 me-2 myGreenBlueColor"/>До неделя</span>
+                        <BsFillCalendarWeekFill className="mb-2 me-2 myGreenBlueColor"/>До неделя</span>
 
                     <div className="d-flex fs-4 mb-2">
                         <div className="me-2">
-                            <FaClock className="mb-1 me-1 myGreenBlueColor"/>
+                            <FaClock className="mb-2 me-2 myGreenBlueColor"/>
                             <span className="fw-medium">От 10:00 ч.</span>
                         </div>
 
                         <span>-</span>
 
                         <div className="ms-2">
-                            <FaClock className="mb-1 me-1 myGreenBlueColor"/>
+                            <FaClock className="mb-2 me-2 myGreenBlueColor"/>
                             <span className="fw-medium">До 20:00 ч.</span>
                         </div>
                     </div>
@@ -45,23 +46,55 @@ function MoreInfo() {
                 </div>
 
                 <div className="companyInfo">
-                    Добре дошли в нашия онлайн магазин за хранителни добавки! Ние сме вашата дестинация за
-                    висококачествени продукти, които подпомагат здравословния ви начин на живот. Ние предлагаме
-                    разнообразие от добавки за вашето благополучие, избрано с грижа за вашия комфорт и удобство.
+                    Добре дошли в нашия
+                    <span className="myGreenBlueColor"> онлайн магазин </span>
+                    за
+                    <span className="myGreenBlueColor"> хранителни добавки</span>
+                    ! Ние сме вашата дестинация за
+                    <span className="myGreenBlueColor"> висококачествени продукти</span>
+                    , които подпомагат
+                    <span className="myGreenBlueColor"> здравословния </span>
+                    ви начин на живот. Ние предлагаме
+                    разнообразие от добавки за вашето
+                    <span className="myGreenBlueColor"> благополучие</span>
+                    , избрано с грижа за вашия
+                    <span className="myGreenBlueColor"> комфорт </span>
+                    и
+                    <span className="myGreenBlueColor"> удобство</span>
+                    .
                     <br/>
                     <br/>
-                    В нашия магазин няма физически обекти - всичко е достъпно само онлайн, за да можете да пазарувате от
-                    всяко място, по всяко време. С нас можете да разгледате богатия ни асортимент, да получите
-                    информация за продуктите и да направите вашите покупки с лекота и увереност.
+                    Нашият магазин няма физически обекти - всичко е
+                    <span className="myGreenBlueColor"> достъпно само онлайн</span>
+                    , за да можете да пазарувате от
+                    <span className="myGreenBlueColor"> всяко място</span>
+                    , по
+                    <span className="myGreenBlueColor"> всяко време</span>
+                    . С нас можете да разгледате
+                    <span className="myGreenBlueColor"> богатия </span>
+                    ни
+                    <span className="myGreenBlueColor"> асортимент</span>
+                    , да получите информация за продуктите и да
+                    <span className="myGreenBlueColor"> направите </span>
+                    вашите
+                    <span className="myGreenBlueColor"> покупки </span>
+                    с
+                    <span className="myGreenBlueColor"> лекота </span>
+                    и
+                    <span className="myGreenBlueColor"> увереност</span>
+                    .
                     <br/>
                     <br/>
-                    Благодарим ви, че избрахте нас за вашия източник на хранителни добавки. Станете част от нашата
-                    общност и започнете пътя си към по-здравословен начин на живот днес!
+                    <span className="myGreenBlueColor">Благодарим ви</span>
+                    , че избрахте нас за вашия
+                    <span className="myGreenBlueColor"> източник </span>
+                    на хранителни добавки. Станете част от нашата
+                    <span className="myGreenBlueColor"> общност </span>
+                    и започнете пътя си към
+                    <span className="myGreenBlueColor"> по-здравословен </span>
+                    начин на живот днес!
                 </div>
-
-
             </div>
-
 
             <ContactUsModal show={showContactUsModal} handleClose={handleContactUsModalClose}/>
         </div>
