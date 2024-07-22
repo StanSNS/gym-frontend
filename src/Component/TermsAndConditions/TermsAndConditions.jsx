1a9ff27fc256312ec85c9154fb1655e96b9308b8
Stanimir Sergev <stanimir.sergev@hubparking.com>
2024-07-22 09:48:04 +0300

    add terms and conditions raw data.

--- src/Component/TermsAndConditions/TermsAndConditions.jsx
+++ src/Component/TermsAndConditions/TermsAndConditions.jsx
@@ -1,5 +1,22 @@
+import {termsAndConditions} from "../../Resources/TermsAndConditions/termsAndConditions";
+import './TermsAndConditions.css'
+
 export default function TermsAndConditions() {
     return (
-        <div>Terms and conds to do</div>
-    )
+        <div className="termsAndCondsSection">
+            <h1 className="termsAndCondsTitle">{termsAndConditions.title}</h1>
+            {termsAndConditions.sections.map((section, sectionIndex) => (
+                <div key={sectionIndex}>
+                    <h2>{section.section}</h2>
+                    <ul>
+                        {section.rules.map((rule) => (
+                            <li key={rule.id}>
+                                <strong>{rule.id}</strong>: {rule.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
 }
